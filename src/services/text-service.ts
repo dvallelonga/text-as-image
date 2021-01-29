700ea29fb56d16da30bfe639385aceea100576aa
David Vallelonga <david.vallelonga@gmail.com>
2021-01-28 22:58:36 -0700

    Strip extra whitespace from input text

--- src/services/text-service.ts
+++ src/services/text-service.ts
@@ -1,3 +1,14 @@
 export async function readContentFromTextUpload(sourceTextUploadFileBuffer: Buffer): Promise<string>{
   return sourceTextUploadFileBuffer.toString();
 }
+
+export function stripWhitespace(text: string): string {
+  let strippedText: string = text
+    .trim()
+    // Condense extra whitespace characters (same characters found
+    // by String.trim()) in a string by replacing each run of one
+    // or more whitespace character with a single Space character.
+    .replace(/[\s\uFEFF\xA0]+/g, ' ');
+
+  return strippedText;
+}
--- test/services/text-service.test.ts
+++ test/services/text-service.test.ts
@@ -1,12 +1,22 @@
 import {
-  readContentFromTextUpload
+  readContentFromTextUpload,
+  stripWhitespace
 } from '../../src/services/text-service';
 import fs from 'fs';
 
 
-
-let EXPECTED_SINGLE_TEXT_FILE_CONTENT = `Lorem ipsum dolor sit amet, 
+const EXPECTED_SINGLE_TEXT_FILE_CONTENT = `Lorem ipsum dolor sit amet, 
 consectetur adipiscing elit.`;
+
+const TEXT_WITH_WHITESPACE_CHARS = `    Lorem ipsum dolor sit amet, 
+consectetur        adipiscing elit. 
+
+\tUt dapibus dignissim ipsum vitae tempor. 
+\t\tClass aptent\ntaciti\rsociosqu ad litora 
+torquent per\tconubia nostra, per inceptos himenaeos.       `;
+
+const EXPECTED_TEXT_WITH_WHITESPACE_CHARS_STRIPPED = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus dignissim ipsum vitae tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`;
+
 
 describe("Parsing of Upload Text", () => {
   it("should process an upload containing a single text file", async () => {
@@ -14,6 +24,11 @@ describe("Parsing of Upload Text", () => {
 
     let textFileContent: string = await readContentFromTextUpload(textUploadFile);
     expect(textFileContent).toEqual(EXPECTED_SINGLE_TEXT_FILE_CONTENT);
+  });
+
+  it("should remove extra whitespace characters from text", async () => {
+    let whitespaceStripped: string = await stripWhitespace(TEXT_WITH_WHITESPACE_CHARS);
+    expect(whitespaceStripped).toEqual(EXPECTED_TEXT_WITH_WHITESPACE_CHARS_STRIPPED);
   });
 
 });
